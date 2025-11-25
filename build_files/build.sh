b5807214f12a5831dbd3c24065584c86cecfaa43
Mikkel Steen Hansen <mikkel@fam-s-hansen.dk>
2025-11-25 12:33:20 +0100

    Adding default programs

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Enable coprs
+dnf5 copr enable dejan/lazygit
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux wget neovim fish lazygit
 
 # Use a COPR Example:
 #
@@ -18,6 +21,13 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Disabling coprs
+dnf5 copr disable dejan/lazygit
+
+# Installing sync software for Mega.io 
+wget https://mega.nz/linux/repo/Fedora_43/x86_64/megasync-Fedora_43.x86_64.rpm && dnf5 install "$PWD/megasync-Fedora_43.x86_64.rpm"
+rm megasync-Fedora_43.x86_64.rpm
 
 #### Example for enabling a System Unit File
 
